9e8e7d28e6cfdf0d9cc1f816acb4a31adc7d858e
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-05-14 14:11:33 +0800

    boom
    boom

---
+++ scripts/main.js
@@ -0,0 +1,53 @@
+Blocks.coreShard.configurable = true;
+Blocks.coreShard.buildType = () => extend(CoreBlock.CoreBuild, Blocks.coreShard, {
+  buildConfiguration(table) {
+    table.button("Send\nto\nHeaven", Styles.flatBordert, () => {
+      this.kill();
+    }).size(160);
+  }
+});
+
+Blocks.coreFoundation.configurable = true;
+Blocks.coreFoundation.buildType = () => extend(CoreBlock.CoreBuild, Blocks.coreFoundation, {
+  buildConfiguration(table) {
+    table.button("Send\nto\nHeaven", Styles.flatBordert, () => {
+      this.kill();
+    }).size(160);
+  }
+});
+
+Blocks.coreNucleus.configurable = true;
+Blocks.coreNucleus.buildType = () => extend(CoreBlock.CoreBuild, Blocks.coreNucleus, {
+  buildConfiguration(table) {
+    table.button("Send\nto\nHeaven", Styles.flatBordert, () => {
+      this.kill();
+    }).size(160);
+  }
+});
+
+Blocks.coreBastion.configurable = true;
+Blocks.coreBastion.buildType = () => extend(CoreBlock.CoreBuild, Blocks.coreBastion, {
+  buildConfiguration(table) {
+    table.button("Send\nto\nHeaven", Styles.flatBordert, () => {
+      this.kill();
+    }).size(160);
+  }
+});
+
+Blocks.coreCitadel.configurable = true;
+Blocks.coreCitadel.buildType = () => extend(CoreBlock.CoreBuild, Blocks.coreCitadel, {
+  buildConfiguration(table) {
+    table.button("Send\nto\nHeaven", Styles.flatBordert, () => {
+      this.kill();
+    }).size(160);
+  }
+});
+
+Blocks.coreAcropolis.configurable = true;
+Blocks.coreAcropolis.buildType = () => extend(CoreBlock.CoreBuild, Blocks.coreAcropolis, {
+  buildConfiguration(table) {
+    table.button("Send\nto\nHeaven", Styles.flatBordert, () => {
+      this.kill();
+    }).size(160);
+  }
+});
